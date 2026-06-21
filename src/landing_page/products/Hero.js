bab4dd75b4a910225d06ddb3081e0bed42c999d9
Yogesh Longwani <yogeshlongwani07@gmail.com>
2026-06-21 20:52:47 +0530

    Update Hero.js

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
             <div className='text-center mt-5 p-5'>
                 <h1>Zerodha Products</h1>
                 <h3 className='text-muted fs-5 mt-3'>Sleek, modern, and intuitive trading platforms</h3>
-                <p className='mt-3 mb-5'>Check out our <a href='/jdsf' style={{textDecoration:"none"}}>investment offerings &nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+                <p className='mt-3 mb-5'>Check out our <a href='/#' style={{textDecoration:"none"}}>investment offerings &nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
             </div>
             <hr></hr>
         </div>
